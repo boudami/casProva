0d04ea9349472d1c450a1df41df435834e5dab0e
Amin <amin.boudlale.stud@ispascalcomandini.it>
2023-03-17 18:40:01 +0100

    Versione 2: primo commit biblioteca

---
+++ gitProva7/Biblioteca.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitProva7
+{
+    internal class Biblioteca
+    {
+        string nome, indirizzo;
+        int orarioApertura, orarioChiusura;
+        public Biblioteca(string nome, string indirizzo, int orarioApertura, int orarioChiusura)
+        {
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            this.orarioApertura = orarioApertura;
+            this.orarioChiusura = orarioChiusura;
+        }
+
+    }
+}
