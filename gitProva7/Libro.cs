dfee32bf7a7a90e2369d5ce0d70affabe72f88b3
Amin <amin.boudlale.stud@ispascalcomandini.it>
2023-03-17 18:26:17 +0100

    Versione 1: classe Libro con metodo toString

---
+++ gitProva7/Libro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitProva7
+{
+    internal class Libro
+    {
+        string nome, cognome;
+        int anno;
+        public Libro(string nome, string cognome, int anno)
+        {
+            this.nome = nome;
+            this.cognome = cognome; 
+            this.anno = anno;   
+        }
+    }
+}
